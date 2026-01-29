60e3111b7bb4ca2f3ae997e1a9828b8f8883c9bb
Fathima <fathima.afi876@gmail.com>
2026-01-29 15:19:46 +0400

    configured redis database connection and updated the port number and host

--- redis_db.py
+++ redis_db.py
@@ -1,14 +1,4 @@
 
-# from redis_om import get_redis_connection
-# import os
-
-# redis = get_redis_connection(
-#     host=os.getenv("redis-19105.crce263.ap-south-1-1.ec2.cloud.redislabs.com", "localhost:5000"),
-#     port=int(os.getenv("11844", 6379)),
-#     password=os.getenv("Q4COyJ7Fe1VnGZc9iRnpZrG3pxXInymE", None),
-#     decode_responses=True
-# )
-
 from redis_om import get_redis_connection
 
 redis = get_redis_connection(
